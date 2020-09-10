02bca1b095b100336565cb3458f86196aabe5a94
Rav317 <raghavb144@gmail.com>
2020-09-10 17:33:46 +0530

    Added check function

--- mapper_t2.py
+++ mapper_t2.py
@@ -5,6 +5,17 @@ import ndjson
 import datetime
 import math
 
+# function to check vallidity of the json object
+def is_valid(section):
+	if(all(c.isalpha() or c.isspace() for c in section['word']) and\
+			len(section['countrycode']) == 2 and 'A'<=section['countrycode'][0]<='Z' and 'A'<=section['countrycode'][1]<='Z' and\
+			(section['recognized'] == True or section['recognized'] == False) and \
+			len(section['key_id']) == 16 and all('0'<=c<='9' for c in section['key_id']) and \
+			len(section['drawing']) >= 1 and all(len(arr) == 2 and len(arr[0]) == len(arr[1]) and all(type(val) == int for val in arr[0]) and all(type(val) == int for val in arr[1]) for arr in section['drawing'])):
+			return True
+	
+	return False
+
 reader = ndjson.reader(sys.stdin)  # ndjson reader for inputting data
 
 # accessing the word argument and the distance argument
@@ -18,12 +29,8 @@ inter_kv = {}
 for section in reader:
 
 	# checking validity of the objects according to the given constraints
-	if(not (all(c.isalpha() or c.isspace() for c in section['word']) and\
-			len(section['countrycode']) == 2 and 'A'<=section['countrycode'][0]<='Z' and 'A'<=section['countrycode'][1]<='Z' and\
-			(section['recognized'] == True or section['recognized'] == False) and \
-			len(section['key_id']) == 16 and all('0'<=c<='9' for c in section['key_id']) and \
-			len(section['drawing']) >= 1 and all(len(arr) == 2 and len(arr[0]) == len(arr[1]) and all(type(val) == int for val in arr[0]) and all(type(val) == int for val in arr[1]) for arr in section['drawing']))):
-			continue
+	if(not is_valid(section)):
+		continue
 
 	# getting the x and y coordinates of the 0th coordinate of the first stroke
 	xcoor = section['drawing'][0][0][0]
