2dd9dcf8165cf21f150e1dbb7d94e2b4aced169c
Ishan Agarwal <54311154+ishan-agarwal@users.noreply.github.com>
2020-11-30 02:10:09 +0530

    Update client.py

--- BD_PROJ/client.py
+++ BD_PROJ/client.py
@@ -1,45 +1,85 @@
-# from socket import *
-# import json
-# import sys
-# serverName = 'localhost'
-# serverPort = 6100
-
-# #serverName = input("Enter server IP: ")
-# #serverPort = int(input("Enter port number: "))
-
-# clientSocket = socket(AF_INET, SOCK_STREAM)
-# clientSocket.connect((serverName,serverPort))
-
-# while(1):
-# 	json_string = clientSocket.recv(socket.CMSG_LEN).decode()
-# 	#print(type(json_string))
-# 	#modifiedSentence = json.loads(json_string)
-# 	print('From Server:', json_string)
-# 	print("\n\n\n\n")
-# clientSocket.close()
-
-
-
-from pyspark import SparkConf, SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.sql import Row, SQLContext
+from __future__ import print_function
 import sys
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
 import json
+from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+from pyspark.sql.types import *
 
-configuration = SparkConf()
-configuration.setAppName('Project')
+def playdels_update(match,event):
+    pass
+def chems_update(match):
+    pass
 
-spark_context = SparkContext(conf = configuration)
-streaming_context = StreamingContext(spark_context,2)
+def driver(rdd):
+    a = rdd.collect()
+    print(type(a))
+    match = json.loads(a[0])
+    for event in a[1:]:
+        event = json.loads(event)
+        playdels_update(match,event)
+    chems_update(match)
 
-streaming_context.checkpoint('Project Checkpoint')
-input_stream = streaming_context.socketTextStream('localhost',6100)
+spark = SparkSession.builder.appName("Proj").master("local[2]").getOrCreate()
+
+schema2 = StructType([
+    
+    StructField('player_1', IntegerType(), True),
+    StructField('player_2', IntegerType(), True),
+    StructField('chemistry', IntegerType(), True),
+]) 
+
+schema = StructType([
+    
+    StructField('match_date', IntegerType(), True),
+    StructField('team_1', IntegerType(), True),
+    StructField('team_2', IntegerType(), True),
+    StructField('score_1', IntegerType(), True),
+    StructField('score_2', IntegerType(), True),
+    
+    StructField('player_Id', IntegerType(), True),
+
+    StructField('accurate_pass', IntegerType(), True),
+    StructField('not_accurate_pass', IntegerType(), True),
+    StructField('key_pass', IntegerType(), True),
+    
+    StructField('duels_lost', IntegerType(), True),
+    StructField('duels_neutral', IntegerType(), True),
+    StructField('duels_won', IntegerType(), True),
+    
+    StructField('no_of_effective_freekicks', IntegerType(), True),
+    StructField('no_of_penalties_scored', IntegerType(), True),
+    StructField('total_no_of_freekicks', IntegerType(), True),
+    
+    StructField('shot_is_on_target', IntegerType(), True),
+    StructField('shot_is_not_on_target', IntegerType(), True),
+    StructField('shot_was_a_goal', IntegerType(), True),
+    
+    StructField('no_of_fouls', IntegerType(), True),
+    
+    StructField('no_of_own_goals', IntegerType(), True),
+    
+    StructField('no_of_goals', IntegerType(), True),
+    
+    StructField('minutes_played', IntegerType(), True),
+    
+    StructField('yellow_card', IntegerType(), True),
+    StructField('red_card', IntegerType(), True),
+    
+  ])
+
+playdels = spark.createDataFrame(spark.sparkContext.emptyRDD(),schema)
+playdels.printSchema()
 
 
-print(input_stream)
+chems = spark.createDataFrame(spark.sparkContext.emptyRDD(),schema2)
+chems.printSchema()
 
-streaming_context.start()
 
-streaming_context.awaitTermination()
-
-streaming_context.stop()
+ssc = StreamingContext(spark.sparkContext, 5)
+dstream = ssc.socketTextStream('localhost', 6100)
+dstream.foreachRDD(driver)
+ssc.start()
+ssc.awaitTermination()
