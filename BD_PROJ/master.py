f356588916ac7b4bc0632be1b7760922dc6e31c6
Rav317 <raghavb144@gmail.com>
2020-12-09 18:53:57 +0530

    Added comments and updated readme

--- BD_PROJ/master.py
+++ BD_PROJ/master.py
@@ -66,14 +66,17 @@ schema = StructType([
 
 playdels = spark.createDataFrame(spark.sparkContext.emptyRDD(),schema)
 
+# converts the datetime string into datetime object
 def to_datetime(x):
 	tempdate = to_timestamp(x.split()[0],"yyyy-MM-dd")
 	return tempdate
 
+# returns the match details based on the match identifiers
 def select_this_match(req_match):
 	ret = playdels.filter(req_match['dateutc'].split()[0]==playdels.match_date).filter(req_match['label']==playdels.label)
 	return ret
 
+# calculates the pass accuracy of the player for a particular match or aggregated
 def return_pass_accuracy(req_player_id,match=0):
 	if(match):
 		np = select_this_match(match).filter(playdels.player_Id==req_player_id).agg({'normal_pass':'sum'}).collect()[0]['sum(normal_pass)']
@@ -87,6 +90,7 @@ def return_pass_accuracy(req_player_id,match=0):
 		akp = playdels.filter(playdels.player_Id==req_player_id).agg({'accurate_key_pass':'sum'}).collect()[0]['sum(accurate_key_pass)']
 	return (anp+(akp*2))/(np+(kp*2))
 
+# calculates the duel effectiveness of the player for a particular match or aggregated
 def return_duel_effectiveness(req_player_id,match=0):
 	if (match):
 		duels_won = select_this_match(match).filter(playdels.player_Id==req_player_id).agg({'duels_won':'sum'}).collect()[0]['sum(duels_won)']
@@ -98,6 +102,7 @@ def return_duel_effectiveness(req_player_id,match=0):
 		duels_lost = playdels.filter(playdels.player_Id==req_player_id).agg({'duels_lost':'sum'}).collect()[0]['sum(duels_lost)']
 	return (duels_won + duels_neutral*0.5)/(duels_won + duels_neutral + duels_lost)
 
+# calculates the free kick effectiveness of the player for a particular match or aggregated
 def return_free_kick_effectiveness(req_player_id,match=0):
 	if (match) :
 		eff_freekicks = select_this_match(match).filter(playdels.player_Id==req_player_id).agg({'no_of_effective_freekicks':'sum'}).collect()[0]['sum(no_of_effective_freekicks)']
@@ -109,6 +114,7 @@ def return_free_kick_effectiveness(req_player_id,match=0):
 		total_freekicks = playdels.filter(playdels.player_Id==req_player_id).agg({'total_no_of_freekicks':'sum'}).collect()[0]['sum(total_no_of_freekicks)']
 	return (eff_freekicks+penalties_scored)/(total_freekicks)
 
+# calculates the number of shots on target of the player for a particular match or aggregated
 def return_shots_on_target(req_player_id,match=0):
 	if(match):
 		shot_on_target_and_goal = select_this_match(match).filter(playdels.player_Id==req_player_id).agg({'shot_on_target_and_goal':'sum'}).collect()[0]['sum(shot_on_target_and_goal)']
@@ -121,14 +127,17 @@ def return_shots_on_target(req_player_id,match=0):
 		total_shots = playdels.filter(playdels.player_Id==req_player_id).agg({'total_shots':'sum'}).collect()[0]['sum(total_shots)']
 	return (shot_on_target_and_goal + shot_on_target_and_not_goal*0.5)/(total_shots)
 
+# calculates the number of fouls of the player for a particular match or aggregated
 def return_no_of_fouls(req_player_id):
 	no_of_fouls = playdels.filter(playdels.player_Id==req_player_id).agg({'no_of_fouls':'sum'}).collect()[0]['sum(no_of_fouls)']
 	return no_of_fouls
 
+# calculates the number of own goals of the player for a particular match or aggregated
 def return_no_of_own_goals(req_player_id):
 	no_of_own_goals = playdels.filter(playdels.player_Id==req_player_id).agg({'no_of_own_goals':'sum'}).collect()[0]['sum(no_of_own_goals)']
 	return no_of_own_goals
 
+# calculates the player contribution for a particular match using above attributes
 def return_player_contribution(req_player_id,match):
 	if(playdels.filter(playdels.player_Id==req_player_id).filter(playdels.label==match["label"]).collect()[0]['minutes_played']==0):
 		return 0
@@ -139,6 +148,7 @@ def return_player_contribution(req_player_id,match):
 		contribution = contribution*(playdels.filter(playdels.player_Id==req_player_id).filter(playdels.label==match["label"]).collect()[0]['minutes_played']/90)
 	return contribution
 
+# calculates the players performance using its contribution and penalties
 def return_player_performance(req_player_id,match):
 	contribution = return_player_contribution(req_player_id,match)
 	foul_penalty = (0.005*playdels.filter(playdels.player_Id==req_player_id).filter(playdels.label==match["label"]).collect()[0]['no_of_fouls'])
@@ -146,19 +156,23 @@ def return_player_performance(req_player_id,match):
 	contribution = contribution - foul_penalty - own_goal_penalty
 	return contribution
 
+# calculates rating of player using performance
 def return_player_rating(req_player_id,match):
 	return (return_player_performance(req_player_id,match)+0.5)/2#player_profile.filter(player_profile.Id == req_player_id).collect()[0]['rating']
 
+# add base values for attributes in playdel dataframe
 def initialise_playdel(match):
 	global playdels
 	columns = list(playdels.columns)
 
+	# getting attribute values from match json
 	datee = match["dateutc"].split(" ")[0]
 	label = match["label"]
 	gameweek = match["gameweek"]
 	venue = match["venue"]
 	duration = match["duration"]
 
+	# adding values for both teams in a match
 	team_ids = list(match['teamsData'].keys())
 	for t_id in team_ids:
 		for players in match["teamsData"][t_id]["formation"]["lineup"]:
@@ -166,7 +180,10 @@ def initialise_playdel(match):
 			init_row = spark.createDataFrame([(datee, label, playerId, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 0, 0, 0, venue,gameweek,duration)], columns)
 			playdels = playdels.union(init_row)
 
+# calculates change in ratings for two players in a match
 def rat_change(id1, id2, match):
+	
+	# getting rating after match and initial rating of the player
 	mat_rat1 = return_player_rating(id_1, match)
 	init_rat1 = player_profile.filter(player_profile.id == id_1).select(player_profile.rating)
 
@@ -180,6 +197,7 @@ def rat_change(id1, id2, match):
 
 	return rat_ch_1, rat_ch_2,ch
 
+# updates the chemistries for all players played in a match
 def chems_update(match):
 	global chems
 	global playdels
@@ -188,9 +206,11 @@ def chems_update(match):
 	team_1 = playdels.filter((playdels.match_date == match["dateutc"].split(" ")[0] )&( playdels.label == match["label"] )&( playdels.team_Id == team_ids[0])).select(playdels.player_Id)
 	team_2 = playdels.filter((playdels.match_date == match["dateutc"].split(" ")[0] )&( playdels.label == match["label"] )&( playdels.team_Id == team_ids[1])).select(playdels.player_Id)
 
+	# getting players for each team
 	team_1_ids = [i['player_Id'] for i in team_1.collect()]
 	team_2_ids = [i['player_Id'] for i in team_2.collect()]
 	
+	# calculating chemistries for players of opposite teams
 	for pl_id_1 in team_1_ids:
 		for pl_id_2 in team_2_ids:
 			ch_1, ch_2,ch = rat_change(pl_id_1, pl_id_2, match)
@@ -218,7 +238,7 @@ def chems_update(match):
 				chem = chem.where("player_1" != pl_id_2 & "player_2" != pl_id_1)
 				new_row  = spark.createDataFrame([(pl_id_2, pl_id_1, new_val)], list(chems.columns))
 
-
+	# calculating chemistries for players of team_1 with themselves
 	for i in range(len(team_1_ids)):
 		for j in range(i+1, len(team_1_ids)):
 			ch_1, ch_2, ch = rat_change(team_1_ids[i], team_1_ids[j])
@@ -245,7 +265,7 @@ def chems_update(match):
 				chem = chem.where("player_1" != team_ids[j] & "player_2" != team_1_ids[i])
 				new_row  = spark.createDataFrame([(team_ids[j], team_1_ids[i], new_val)], list(chems.columns))
 
-
+	# calculating chemistries for players of team_2 with themselves
 	for i in range(len(team_2_ids)):
 		for j in range(i+1, len(team_2_ids)):
 			ch_1, ch_2, ch = rat_change(team_2_ids[i], team_2_ids[j])
@@ -272,6 +292,7 @@ def chems_update(match):
 				chem = chem.where("player_1" != team_ids[j] & "player_2" != team_1_ids[i])
 				new_row  = spark.createDataFrame([(team_ids[j], team_1_ids[i], new_val)], list(chems.columns))
 
+# updates the attributes for each player after each event
 def handle_event(event,match):
 	global playdels
 	req_player_id = int(event['playerId'])
@@ -279,7 +300,9 @@ def handle_event(event,match):
 		pass
 	try:
 		dateutc = match['dateutc'].split()[0]
-		if(event['eventId']==8):#Pass
+
+		# Pass
+		if(event['eventId']==8):
 			np  = select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['normal_pass']
 			anp = select_this_match(match).filter(playdels.player_Id==req_player_id).agg({'accurate_normal_pass':'sum'}).collect()[0]['sum(accurate_normal_pass)']
 			kp  = select_this_match(match).filter(playdels.player_Id==req_player_id).agg({'key_pass':'sum'}).collect()[0]['sum(key_pass)']
@@ -301,6 +324,8 @@ def handle_event(event,match):
 			# print(select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['accurate_normal_pass'])
 			# print(select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['key_pass'])
 			# print(select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['accurate_key_pass'])
+		
+		# Duels
 		elif(event['eventId']==1):
 			# print("player id : ",req_player_id)
 			# print(select_this_match(match).show())
@@ -318,6 +343,8 @@ def handle_event(event,match):
 			playdels = playdels.withColumn("duels_lost",    when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), duel_l).otherwise(playdels["duels_lost"]))
 			playdels = playdels.withColumn("duels_neutral", when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), duel_n).otherwise(playdels["duels_neutral"]))
 			playdels = playdels.withColumn("duels_won",     when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), duel_w).otherwise(playdels["duels_won"]))
+		
+		# Free kick
 		elif(event['eventId']==3):
 			effective_f=select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['no_of_effective_freekicks']
 			total_f=select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['total_no_of_freekicks']
@@ -339,6 +366,7 @@ def handle_event(event,match):
 			playdels = playdels.withColumn("total_no_of_freekicks",           when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), total_f).otherwise(playdels["total_no_of_freekicks"]))
 			playdels = playdels.withColumn("no_of_goals",           when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), goals).otherwise(playdels["no_of_goals"]))
 
+		# Shot on target
 		elif(event['eventId']==10):
 			shot_t_g=select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['shot_on_target_and_goal']
 			shot_t_ng=select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['shot_on_target_and_not_goal']
@@ -360,10 +388,14 @@ def handle_event(event,match):
 			playdels = playdels.withColumn("shot_on_target_and_not_goal",           when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), shot_t_ng).otherwise(playdels["shot_on_target_and_not_goal"]))
 			playdels = playdels.withColumn("total_shots",           when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), shots_t).otherwise(playdels["total_shots"]))
 			playdels = playdels.withColumn("no_of_goals",           when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), goals).otherwise(playdels["no_of_goals"]))
+		
+		# Foul loss
 		elif(event['eventId']==2):
 				foul=select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['no_of_fouls']
 				foul+=1
 				playdels = playdels.withColumn("no_of_fouls",           when((playdels["player_Id"] == req_player_id) & (playdels["match_date"] == dateutc) & (playdels["label"] == match['label']), fouls).otherwise(playdels["no_of_fouls"]))
+		
+		# Own goal
 		elif(event['eventId']==102):
 				og=select_this_match(match).filter(playdels.player_Id==req_player_id).collect()[0]['no_of_own_goals']
 				og+=1
@@ -371,57 +403,80 @@ def handle_event(event,match):
 	except:
 		pass
 
+
+# driver code
 def driver(rdd):
     a = rdd.collect()
     match = json.loads(a[0])
+    # initialising the event details for each player involved in the match
     initialise_playdel(match)
     playdels.show(50)
+
     for event in a[1:]:
         # print("event")
         event = json.loads(event)
+
+        # updating attribute values after each event
         handle_event(event,match)
         print(" ------- Player Details after an event ------- ")
         playdels.show()
+
+    # updating the chemistries after each match
     chems_update(match)
     chems.show()
 
-# player profile
+# getting player profile
 player_profile = spark.read.options(header='True').csv("file:///home/ishan/Desktop/BD_PROJ/play.csv")
 player_profile = player_profile.withColumn("no_of_fouls",lit(0)).withColumn("no_of_goals",lit(0)).withColumn("no_of_own_goals",lit(0)).withColumn("pass_accuracy",lit(0)).withColumn("shots_on_target",lit(0)).withColumn("no_of_matches_played",lit(0)).withColumn("rating",lit(0.5))
 player_profile.printSchema()
 
+# getting chemistries
 chems = spark.read.options(header='True').csv("file:///home/ishan/Desktop/BD_PROJ/chems.csv")
 chems.printSchema()
 
-
+# training the regression model
 def regression_train(match_date):
     global player_profile
     global lrModel
     match_date=datetime.strptime(match_date,"%Y-%m-%d")
 
+    # calculating the age of players on the match day
     input_df = player_profile.withColumn("birthDate", player_profile["birthDate"].cast(DateType()))
     df_reg=input_df.select("name","birthDate","rating")
     df_reg=df_reg.withColumn("Age", months_between(lit(match_date),col("birthDate"))/12)
+
+    # adding square of the age values for 2nd degree regression
     df_reg=df_reg.withColumn("Age_Sq",(col("Age")*col("Age")))
     df_reg.show()
 
+    # creating the input feature vector
     assembler = VectorAssembler(inputCols=["Age", "Age_Sq"],outputCol="features")
     df = assembler.transform(df_reg)
+
+    # initializing the model architecture
     lr = LinearRegression(featuresCol = "features", labelCol = "rating", maxIter=10, regParam=0.3, elasticNetParam=0.8)
+    
+    # fitting the model to the data
     lrModel = lr.fit(df)
 
+    # metrics of the model
     print("Coefficients: %s" % str(lrModel.coefficients))
     print("Intercept: %s" % str(lrModel.intercept))
 
 def regression_test(match_date, birth_date):
     global lrModel
+
+    # getting the age of the player on the match date
     birth_date=datetime.strptime(birth_date,"%Y-%m-%d")
     diff = relativedelta(match_date, birth_date)
     diff_years = diff.years + diff.months/12 + diff.days/365
     test_df = spark.createDataFrame([(diff_years, diff_yearsdiff_years)], schema_test)
 
+    # adding feature vector
     assembler = VectorAssembler(inputCols=["Age", "Age_Sq"],outputCol="features")
     test = assembler.transform(test_df)
+    
+    # getting the predicted value
     pred = lrModel.transform(test)
     return pred['rating']
 
@@ -468,9 +523,13 @@ def clustering():
 
 clustering()
 
-
+# initalizing the spark
 ssc = StreamingContext(spark.sparkContext, 5)
+
+# creating the dstream object
 dstream = ssc.socketTextStream('localhost', 6100)
+
+# operating on each rdds
 dstream.foreachRDD(driver)
 ssc.start()
 ssc.awaitTermination()
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Big Data Assignment
 
 This is the repository for the Big Data Assignments for the 3rd Year elective course on Big Data.
+
+# FPL Project
+
+1) Open one terminal and run:
+```
+python3 stream.py
+```
+
+2) On new terminal window/tab run:
+```
+spark-submit master.py
+```
